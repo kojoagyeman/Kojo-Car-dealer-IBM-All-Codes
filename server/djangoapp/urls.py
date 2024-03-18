ccb9e38a07d670126edc8167c2a1d6911bf01461
kojoagyeman <152467258+kojoagyeman@users.noreply.github.com>
2024-03-17 22:13:07 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,4 +20,6 @@ path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
 
  
     # path for add a review view
+     path(route='add_review', view=views.add_review, name='add_review'),
+ 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
